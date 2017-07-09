8a02730bdb0b63660528f43c6940a1febae8ae2e
unnamed42 <a390517426@gmail.com>
2017-07-10 00:51:39 +0800

    IPGW page

--- NeuOldDriver/Models/IPGWModel.cs
+++ NeuOldDriver/Models/IPGWModel.cs
@@ -1,9 +1,36 @@
-﻿namespace NeuOldDriver.Models {
+﻿using System.Threading.Tasks;
+using System.ComponentModel;
 
-    public class IPGWModel {
+namespace NeuOldDriver.Models {
+
+    public class IPGWModel : INotifyPropertyChanged {
         private bool loggedIn = false;
 
-        public bool IsLoggedIn { get { return loggedIn; } }
+        private int  used;
+        private long usedTime;
+
+        public bool NotLoggedIn { get { return !loggedIn; } }
+
+        public int Used { get { return used; } }
+
+        public long UsedTime { get { return UsedTime; } }
+
+        public void DoLogin(LoginData data) {
+            loggedIn = true;
+            NotifyPropertyChanged("NotLoggedIn");
+            Refresh();
+        }
+
+        public void Refresh() {
+            NotifyPropertyChanged("Used");
+            NotifyPropertyChanged("UsedTime");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void NotifyPropertyChanged(string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
 }
--- NeuOldDriver/Pages/IPGW.xaml.cs
+++ NeuOldDriver/Pages/IPGW.xaml.cs
@@ -1,18 +1,18 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+using NeuOldDriver.Models;
 
 namespace NeuOldDriver.Pages {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
+
     public sealed partial class IPGW : Page {
+
         public IPGW() {
             this.InitializeComponent();
         }
 
-        private void Login_Finished(object sender, Models.LoginData e) {
-            split.IsPaneOpen = false;
+        private void DoLogin(object sender, LoginData e) {
+            data.DoLogin(e);
+            
         }
     }
 }
