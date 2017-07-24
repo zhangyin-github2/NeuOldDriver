ca777c800e9c86b4ade568e1486fa62c55026c0d
tu he <fred123@vip.qq.com>
2017-07-24 10:14:09 +0800

    teacherRating page

--- NeuOldDriver/Pages/AAOSubpage/TeacherRating.xaml.cs
+++ NeuOldDriver/Pages/AAOSubpage/TeacherRating.xaml.cs
@@ -23,5 +23,26 @@ namespace NeuOldDriver.Pages.AAOSubPage {
         public TeacherRating() {
             this.InitializeComponent();
         }
+       
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (tBlockKeyword.Text == "徐久强")
+                this.word.Text = "徐久强       所任课程：计算机系统专题        推荐指数：⭐⭐⭐";
+            if (tBlockKeyword.Text == "毕远国")
+                this.word.Text = "毕远国       所任课程：网络新技术        推荐指数：⭐⭐⭐⭐";
+            if (tBlockKeyword.Text == "聂铁铮")
+                this.word.Text = "聂铁铮       所任课程：数据库管理系统实现技术        推荐指数：⭐⭐⭐";
+            if (tBlockKeyword.Text == "魏阳杰")
+                this.word.Text = "魏阳杰       所任课程：专业外语        推荐指数：⭐⭐⭐";
+            if (tBlockKeyword.Text == "李芳芳")
+                this.word.Text = "李芳芳       所任课程：数据库系统实践        推荐指数：⭐⭐";
+            if (tBlockKeyword.Text == "刘铮")
+                this.word.Text = "刘铮      所任课程：计算机网络技术       推荐指数：⭐";
+            else
+                this.word.Text = "未查询到您要查找的教师信息";
+
+        }
     }
 }
+
+
