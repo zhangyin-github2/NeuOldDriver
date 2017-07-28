3cddff1fbd7afbe9183f300bfee0fccdfaa89a80
unnamed42 <a390517426@gmail.com>
2017-07-28 14:57:48 +0800

    global initialization fixes

--- NeuOldDriver/Source/Global/Globals.cs
+++ NeuOldDriver/Source/Global/Globals.cs
@@ -7,12 +7,23 @@ using NeuOldDriver.Storage;
 namespace NeuOldDriver.Global {
 
     public static class Globals {
+        
+        // used for synchronization
+        private static volatile bool initialized = false;
+        private static Accounts accounts;
 
-        public static Accounts Accounts { get; private set; }
+        public static Accounts Accounts {
+            get {
+                while (!initialized) ;
+                return accounts;
+            }
+        }
+
         public static HttpClient Client { get; } = new HttpClient();
 
         public static async Task Initialize() {
-            Accounts = await Storage.Accounts.Read();
+            accounts = await Storage.Accounts.Read();
+            initialized = true;
         }
 
         public static async Task Dispose() {
